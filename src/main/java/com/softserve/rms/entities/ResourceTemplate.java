516da8f77beb8188060edddaf2653f234cedfb39
maryshchur <marichka02700@gmail.com>
2020-01-23 18:38:05 +0200

    fixed application properties file

--- src/main/java/com/softserve/rms/entities/ResourceTemplate.java
+++ src/main/java/com/softserve/rms/entities/ResourceTemplate.java
@@ -13,7 +13,6 @@ public class ResourceTemplate {
     private String name;
     @Column(name = "description")
     private String description;
-    //TODO mapping on users table
     @ManyToOne (fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
---
+++ src/main/java/com/softserve/rms/entities/User.java
@@ -0,0 +1,50 @@
+package com.softserve.rms.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+@EqualsAndHashCode(
+      exclude = {"resource_templates"})
+@ToString(
+     exclude = {"resource_templates"})
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name ="first_name", nullable = false, length = 50)
+    private String firstName;
+
+    @Column(name ="last_name", nullable = false, length = 50)
+    private String lastName;
+
+    @Column(nullable = false,unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String phone;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false)
+    private String status;
+//TODO mapping on roles table
+//    @ManyToOne (fetch = FetchType.LAZY)
+//    @JoinColumn(name = "role_id")
+//    private Role role;
+
+    @OneToMany(mappedBy = "user", orphanRemoval = true)
+    private List<ResourceTemplate> resource_templates = new ArrayList<>();
+
+}
