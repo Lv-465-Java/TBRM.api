e189a7db3d616f8a292b3a8aeda08b5722e28977
Andrii Bren <43859320+billyxd3@users.noreply.github.com>
2020-01-23 21:38:24 +0200

    Add environment variable (#5)

--- src/main/java/com/softserve/rms/entities/ResourceParameter.java
+++ src/main/java/com/softserve/rms/entities/ResourceParameter.java
@@ -1,23 +1,28 @@
 package com.softserve.rms.entities;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Entity(name = "resource_parameters")
+@Data
 public class ResourceParameter {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+
     private Long id;
-    @Column(name = "name")
+
     private String name;
-    @Column(name = "type_name")
+
+    @Column(name = "table_name")
     private String typeName;
     @Column(name = "field_type")
     private String fieldType;
-    @Column(name = "pattern")
+
     private String pattern;
+
     private String tableName;
-    @JoinColumn(name = "resource_id")
+
     @ManyToOne
     private ResourceTemplate resourceTemplate;
 }
