169a8a5cb1f934bba54d29a2b52030622d7779cc
maryshchur <marichka02700@gmail.com>
2020-01-28 17:19:31 +0200

    implemented save method

--- src/main/java/com/softserve/rms/service/PersonServiceImpl.java
+++ src/main/java/com/softserve/rms/service/PersonServiceImpl.java
@@ -1,5 +1,9 @@
 package com.softserve.rms.service;
 
+import com.softserve.rms.constant.ErrorMessage;
+import com.softserve.rms.dto.PersonDto;
+import com.softserve.rms.entities.Person;
+import com.softserve.rms.exception.NotSavedException;
 import com.softserve.rms.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +12,23 @@ import org.springframework.stereotype.Service;
 public class PersonServiceImpl implements PersonService {
     @Autowired
     private PersonRepository personRepository;
+
+    private Person convertDtoToPerson(PersonDto personDto){
+        return Person.builder().
+                firstName(personDto.getFirstName()).
+                lastName(personDto.getLastName()).
+                email(personDto.getEmail()).
+                phone(personDto.getPhone()).
+                //password(passwordEncoder.encode(personDto.getPassword())).
+                        password(personDto.getPassword()).
+                        build();
+    }
+
+    public boolean save(PersonDto person) {
+        if(personRepository.save(convertDtoToPerson(person))==null){
+            new NotSavedException(ErrorMessage.USER_NOT_SAVED);
+            return false;
+        } else
+            return true;
+    }
 }
