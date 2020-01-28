7eb12e749d626d23d23a652267f370506b38c468
maryshchur <marichka02700@gmail.com>
2020-01-28 17:02:30 +0200

    created PersonDto class

---
+++ src/main/java/com/softserve/rms/dto/PersonDto.java
@@ -0,0 +1,29 @@
+package com.softserve.rms.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonDto {
+    @NotNull
+    @NotEmpty
+    private String firstName;
+    @NotNull
+    @NotEmpty
+    private String lastName;
+    @NotNull
+    @NotEmpty
+    private String email;
+    @NotNull
+    @NotEmpty
+    private String phone;
+    @NotNull
+    @NotEmpty
+    private String password;
+    private String passwordConfirm;
+}
