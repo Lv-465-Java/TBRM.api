5ab4fa89dfd3202b5c2cb5a64c2d27b02a1722be
maryshchur <marichka02700@gmail.com>
2020-01-28 17:06:18 +0200

    created constant in ErrorMessage

---
+++ src/main/java/com/softserve/rms/constant/ErrorMessage.java
@@ -0,0 +1,8 @@
+package com.softserve.rms.constant;
+
+public class ErrorMessage {
+    public static final String USER_WITH_EMAIL_EXIST = "Account with this email already exist ";
+    public static final String USER_NOT_SAVED = "User was not saved ";
+    public static final String PASSWORD_NOT_MATCHES = "Confirmation password not matches ";
+    public static final String EMPTY_FIELD = "All fields should not be empty ";
+}
