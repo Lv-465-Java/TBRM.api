ef9a5b91293513c9b6da6a544a576ae12c3c42d3
maryshchur <marichka02700@gmail.com>
2020-01-28 17:04:02 +0200

    added save method

--- src/main/java/com/softserve/rms/service/PersonService.java
+++ src/main/java/com/softserve/rms/service/PersonService.java
@@ -1,8 +1,10 @@
 package com.softserve.rms.service;
 
+import com.softserve.rms.dto.PersonDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface PersonService {
+    boolean save(PersonDto person);
 
 }
