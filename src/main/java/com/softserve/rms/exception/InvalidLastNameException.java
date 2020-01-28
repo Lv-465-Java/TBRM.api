2ed928d1e7f40ded2f9b8bb8822cd10496448da0
maryshchur <marichka02700@gmail.com>
2020-01-28 17:39:48 +0200

    added InvalidLastNameException

---
+++ src/main/java/com/softserve/rms/exception/InvalidLastNameException.java
@@ -0,0 +1,7 @@
+package com.softserve.rms.exception;
+
+public class InvalidLastNameException extends RuntimeException{
+    public InvalidLastNameException(String message) {
+        super(message);
+    }
+}
