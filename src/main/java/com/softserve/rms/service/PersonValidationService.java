14172e031689f5420354bd95515fbf42caf1974a
maryshchur <marichka02700@gmail.com>
2020-01-28 17:25:19 +0200

    added emailExist method

--- src/main/java/com/softserve/rms/service/PersonValidationService.java
+++ src/main/java/com/softserve/rms/service/PersonValidationService.java
@@ -1,9 +1,11 @@
 package com.softserve.rms.service;
 
+import com.softserve.rms.entities.Person;
 import com.softserve.rms.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,4 +15,11 @@ public class PersonValidationService {
     private PersonRepository personRepository;
     private Matcher matcher;
     private Pattern pattern;
+
+    public boolean emailExist(String email) {
+        List<Person> people = personRepository.findByEmail(email);
+        if (!people.isEmpty()){
+            return true;
+        } else return false;
+    }
 }
