bb27f0620983933268fc25394d19ba14343e04d2
maryshchur <marichka02700@gmail.com>
2020-01-28 17:08:09 +0200

    added constants

---
+++ src/main/java/com/softserve/rms/constant/ValidationConstants.java
@@ -0,0 +1,19 @@
+package com.softserve.rms.constant;
+
+public class ValidationConstants {
+    public static final String EMPTY_FIRSTNAME = "FirstName field can not be empty";
+    public static final String INVALID_FIRSTNAME = "FirstName field is not valid";
+
+    public static final String EMPTY_LASTNAME = "LastName field can not be empty";
+    public static final String INVALID_LASTNAME = "LastName field is not valid";
+
+    public static final String EMPTY_EMAIL = "Email field can not be empty";
+    public static final String INVALID_EMAIL = "Email is invalid";
+
+    public static final String EMPTY_PHONE = "Phone field can not be empty";
+    public static final String INVALID_PHONE = "Phone number is invalid";
+    //public static final String EXIST_PHONE = "Account with this phone number already exist";
+
+    public static final String INVALID_PASSWORD = "Password must contain at least eight characters and at least one character of "
+            + " uppercase letter, lowercase letter, digit, special character";
+}
