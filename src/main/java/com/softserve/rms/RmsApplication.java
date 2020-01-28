c45cebbf604d790542f1357b931cf7e7bb0ec82e
maryshchur <marichka02700@gmail.com>
2020-01-28 18:01:34 +0200

    added anotation for swagger

--- src/main/java/com/softserve/rms/RmsApplication.java
+++ src/main/java/com/softserve/rms/RmsApplication.java
@@ -2,7 +2,11 @@ package com.softserve.rms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@ComponentScan
+@EnableSwagger2
 @SpringBootApplication
 public class RmsApplication {
 
