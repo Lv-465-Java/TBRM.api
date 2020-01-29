8f1bfcf49f56d8b2ac86f8ddf65dd43d27a960b1
maryshchur <marichka02700@gmail.com>
2020-01-29 12:23:04 +0200

    added anotation for http return code

--- src/main/java/com/softserve/rms/exception/InvalidUserRegistrationDataException.java
+++ src/main/java/com/softserve/rms/exception/InvalidUserRegistrationDataException.java
@@ -1,12 +1,15 @@
 package com.softserve.rms.exception;
 
-import org.json.JSONObject;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.Map;
-
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidUserRegistrationDataException extends RuntimeException {
 
-    public InvalidUserRegistrationDataException(String message) {
+    public InvalidUserRegistrationDataException(String message)
+    {
         super(message);
     }
     public InvalidUserRegistrationDataException(Map<String, String> messages) {
