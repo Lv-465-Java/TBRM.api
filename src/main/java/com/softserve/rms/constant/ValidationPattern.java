cc0ac7350791897cc64d6da71c5f462e6ac40a9a
maryshchur <marichka02700@gmail.com>
2020-01-29 16:50:48 +0200

    put all constants into interface(not class with static field)

--- src/main/java/com/softserve/rms/constant/ValidationPattern.java
+++ src/main/java/com/softserve/rms/constant/ValidationPattern.java
@@ -1,10 +1,10 @@
 package com.softserve.rms.constant;
 
-public class ValidationPattern {
+public interface ValidationPattern {
 
-    public static final String PASSWORD_PATTERN= "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[~`!@#$*%^&(-_)/><?\"|+=:])[A-Za-z\\d~`!@#*$%^&(-_)/><?\"|+=:]{8,}$";
-    public static final String EMAIL_PATTERN="^[a-zA-Z0-9]+((\\.|_|-)?[a-zA-Z0-9])+@[a-zA-Z0-9]+\\.[a-zA-Z]{2,4}$";
-    public static final String PHONE_PATTERN="^\\+[0-9]{12}$";
-    public static final String NAME_PATTERN="^([A-Za-z]+((-|')[A-Za-z]+)*){2,}$";
+    String PASSWORD_PATTERN= "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[~`!@#$*%^&(-_)/><?\"|+=:])[A-Za-z\\d~`!@#*$%^&(-_)/><?\"|+=:]{8,}$";
+    String EMAIL_PATTERN="^[a-zA-Z0-9]+((\\.|_|-)?[a-zA-Z0-9])+@[a-zA-Z0-9]+\\.[a-zA-Z]{2,4}$";
+    String PHONE_PATTERN="^\\+[0-9]{12}$";
+    String NAME_PATTERN="^([A-Za-z]+((-|')[A-Za-z]+)*){2,}$";
 
 }
