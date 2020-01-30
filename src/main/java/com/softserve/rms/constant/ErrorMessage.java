5e46b4b6c10bb03d6a349057a692289060ebfba9
maryshchur <marichka02700@gmail.com>
2020-01-30 11:50:22 +0200

    added error message if user not found by email

--- src/main/java/com/softserve/rms/constant/ErrorMessage.java
+++ src/main/java/com/softserve/rms/constant/ErrorMessage.java
@@ -5,4 +5,5 @@ public interface ErrorMessage {
     String USER_NOT_SAVED = "User was not saved ";
     String PASSWORD_NOT_MATCHES = "Confirmation password not matches ";
     String EMPTY_FIELD = "All fields should not be empty ";
+    String USER_NOT_FOUND_BY_EMAIL = "User was not found by email ";
 }
