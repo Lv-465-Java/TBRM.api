bcb9437e29a824416dbec73940da8b57726486b8
sydor_artur <sydor.artur@ukr.net>
2020-01-30 12:15:57 +0200

    add custom UserDetailsService implementation

--- src/main/java/com/softserve/rms/repository/UserRepository.java
+++ src/main/java/com/softserve/rms/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.softserve.rms.repository;
 
 import com.softserve.rms.entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<Person, Long> {
     Person findPersonByEmail(String email);
 }
