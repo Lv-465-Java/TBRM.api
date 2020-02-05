fed7cce72c75f4031c406081895f6da52add08e3
MarianDutchyn <marian.dutchyn@ukr.net>
2020-02-05 18:39:57 +0200

    add method security for ResourceTemplate

--- src/main/java/com/softserve/rms/repository/ResourceTemplateRepository.java
+++ src/main/java/com/softserve/rms/repository/ResourceTemplateRepository.java
@@ -2,6 +2,8 @@ package com.softserve.rms.repository;
 
 import com.softserve.rms.entities.ResourceTemplate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +12,12 @@ import java.util.Optional;
 @Repository
 public interface ResourceTemplateRepository extends JpaRepository<ResourceTemplate, Long> {
 
+    @PreAuthorize("hasPermission(#id, 'com.softserve.rms.entities.ResourceTemplate', read)")
+    Optional<ResourceTemplate> findById(Long id);
+
+    @PostFilter("hasPermission(filterObject, 'read')")
+    List<ResourceTemplate> findAll();
+
     /**
      * Method finds list of{@link ResourceTemplate} by user id.
      *
@@ -17,6 +25,7 @@ public interface ResourceTemplateRepository extends JpaRepository<ResourceTempla
      * @return list of {@link ResourceTemplate}
      * @author Halyna Yatseniuk
      */
+    @PostFilter("hasPermission(filterObject, 'read')")
     List<ResourceTemplate> findAllByUserId(Long id);
 
     /**
@@ -27,6 +36,7 @@ public interface ResourceTemplateRepository extends JpaRepository<ResourceTempla
      * @return list of {@link ResourceTemplate}
      * @author Halyna Yatseniuk
      */
+    @PostFilter("hasPermission(filterObject, 'read')")
     List<ResourceTemplate> findByNameContainsIgnoreCaseOrDescriptionContainsIgnoreCase(String name, String description);
 
     /**
@@ -37,4 +47,7 @@ public interface ResourceTemplateRepository extends JpaRepository<ResourceTempla
      * @author Halyna Yatseniuk
      */
     Optional<ResourceTemplate> findByName(String name);
+
+    @PreAuthorize("hasPermission(#resourceTemplate, write)")
+    ResourceTemplate save(ResourceTemplate resourceTemplate);
 }
