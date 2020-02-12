f07c28a752022fff0fc13ed97ad485a00c5a9987
halinayatseniuk <halina.yatseniuk@gmail.com>
2020-02-12 18:03:46 +0200

    changed parameter type

--- src/main/java/com/softserve/rms/entities/ParameterType.java
+++ src/main/java/com/softserve/rms/entities/ParameterType.java
@@ -1,18 +1,21 @@
 package com.softserve.rms.entities;
 
+import org.jooq.DataType;
+import org.jooq.impl.SQLDataType;
+
 public enum ParameterType {
-    POINT_INT("Point", "Integer", "integer"),
-    POINT_DOUBLE("Point", "Double", "numeric"),
-    POINT_STRING("Point", "String", "varchar"),
-    RANGE_DOUBLE("Range", "Double", "numeric"),
-    RANGE_INT("Range", "Integer", "integer"),
-    AREA_DOUBLE("Area", "Double", "numeric");
+    POINT_INT("Point", "Integer", SQLDataType.INTEGER),
+    POINT_DOUBLE("Point", "Double", SQLDataType.DOUBLE),
+    POINT_STRING("Point", "String", SQLDataType.VARCHAR),
+    RANGE_DOUBLE("Range", "Double", SQLDataType.DOUBLE),
+    RANGE_INT("Range", "Integer", SQLDataType.INTEGER),
+    AREA_DOUBLE("Area", "Double", SQLDataType.DOUBLE);
 
     private String name;
     private String type;
-    private String sqlType;
+    private DataType sqlType;
 
-    ParameterType(String name, String type, String sqlType) {
+    ParameterType(String name, String type, DataType sqlType) {
         this.name = name;
         this.type = type;
         this.sqlType = sqlType;
@@ -26,7 +29,7 @@ public enum ParameterType {
         return type;
     }
 
-    public String getSqlType() {
+    public DataType getSqlType() {
         return sqlType;
     }
 }
