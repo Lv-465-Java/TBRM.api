423012b6c33db8498635c37f004fc4beae8ab883
Andriy Bren <andriybren5@gmail.com>
2020-02-27 01:44:56 +0200

    Implement methods in ResourceTemplateController class

--- src/main/java/com/softserve/rms/controller/ResourceTemplateController.java
+++ src/main/java/com/softserve/rms/controller/ResourceTemplateController.java
@@ -42,16 +42,130 @@ public class ResourceTemplateController implements ResourceTemplateControllerApi
         this.resourceTemplateService = resourceTemplateService;
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @author Halyna Yatseniuk
+     */
     @Override
     public ResponseEntity<ResourceTemplateDTO> saveTemplate(ResourceTemplateSaveDTO templateDTO) {
         LOG.info("Creating a new Resource Template");
         return ResponseEntity.status(HttpStatus.CREATED).body(resourceTemplateService.save(templateDTO));
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @author Halyna Yatseniuk
+     */
     @Override
     public ResponseEntity<ResourceTemplateDTO> findTemplateById(Long templateId) {
         LOG.info("Getting Resource Template by ID: " + templateId);
         return ResponseEntity.status(HttpStatus.OK).body(resourceTemplateService.findDTOById(templateId));
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @author Halyna Yatseniuk
+     */
+    @Override
+    public ResponseEntity<List<ResourceTemplateDTO>> findAllTemplates() {
+        LOG.info("Getting all Resource Templates");
+        return ResponseEntity.status(HttpStatus.OK).body(resourceTemplateService.getAll());
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @author Halyna Yatseniuk
+     */
+    @Override
+    public ResponseEntity<List<ResourceTemplateDTO>> findAllPublishedTemplates() {
+        LOG.info("Getting all published Resource Templates");
+        return ResponseEntity.status(HttpStatus.OK).body(resourceTemplateService.findAllPublishedTemplates());
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @author Halyna Yatseniuk
+     */
+    @Override
+    public ResponseEntity<List<ResourceTemplateDTO>> findAllTemplatesByUserId(Long userId) {
+        LOG.info("Getting all Resource Templates by user ID: " + userId);
+        return ResponseEntity.status(HttpStatus.OK).body(resourceTemplateService.getAllByUserId(userId));
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @author Halyna Yatseniuk
+     */
+    @Override
+    public ResponseEntity<ResourceTemplateDTO> updateTemplateById(Long templateId, Map<String, Object> body) {
+        LOG.info("Updating Resource Template by ID: " + templateId);
+        return ResponseEntity.status(HttpStatus.OK).body(resourceTemplateService.checkIfTemplateCanBeUpdated(templateId, body));
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @author Halyna Yatseniuk
+     */
+    @Override
+    public ResponseEntity<Object> deleteTemplateById(Long templateId) {
+        LOG.info("Deleting Resource Template by ID: " + templateId);
+        resourceTemplateService.checkIfTemplateCanBeDeleted(templateId);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @author Halyna Yatseniuk
+     */
+    @Override
+    public ResponseEntity<List<ResourceTemplateDTO>> searchTemplateByNameOrDescription(String searchedWord) {
+        LOG.info("Search a Resource Template by name or description contains: " + searchedWord);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(resourceTemplateService.searchByNameOrDescriptionContaining(searchedWord));
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @author Halyna Yatseniuk
+     */
+    @Override
+    public ResponseEntity<Boolean> publishResourceTemplate(Long templateId, Map<String, Object> body) {
+        LOG.info("Publish a Resource Template by ID: " + templateId);
+        resourceTemplateService.selectPublishOrCancelPublishAction(templateId, body);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @Override
+    public ResponseEntity<List<PrincipalPermissionDto>> getUsersWithAccess(String id) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(resourceTemplateService.findPrincipalWithAccessToResourceTemplate(Long.parseLong(id)));
+    }
+
+    @Override
+    public ResponseEntity<PermissionDto> addPermissionToResourceTemplate(PermissionDto permissionDto, Principal principal) {
+        resourceTemplateService.addPermissionToResourceTemplate(permissionDto, principal);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @Override
+    public ResponseEntity<ChangeOwnerDto> changeOwnerForResourceTemplate(ChangeOwnerDto changeOwnerDto, Principal principal) {
+        resourceTemplateService.changeOwnerForResourceTemplate(changeOwnerDto, principal);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @Override
+    public ResponseEntity<Object> deleteAceForCertainUser(PermissionDto permissionDto, Principal principal) {
+        resourceTemplateService.closePermissionForCertainUser(permissionDto, principal);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
 //    /**
