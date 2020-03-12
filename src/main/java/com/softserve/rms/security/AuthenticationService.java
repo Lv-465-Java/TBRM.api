aac85e4e88053ab70566757f247f236263b37d7b
Kravets Maryana <marjana_s@ukr.net>
2020-03-12 21:20:24 +0200

    add test methods

--- src/main/java/com/softserve/rms/security/AuthenticationService.java
+++ src/main/java/com/softserve/rms/security/AuthenticationService.java
@@ -1,6 +1,5 @@
 package com.softserve.rms.security;
 
-import com.amazonaws.services.kms.model.DisabledException;
 import com.softserve.rms.dto.JwtDto;
 import com.softserve.rms.dto.LoginUser;
 import com.softserve.rms.entities.User;
@@ -11,6 +10,7 @@ import com.softserve.rms.service.implementation.UserServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
