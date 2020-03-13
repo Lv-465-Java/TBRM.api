7ba2a18fc7b2504a0e20d5e589e0ddd1bab73850
Marian Dutchyn <marian.dutchyn@ukr.net>
2020-03-13 13:12:52 +0200

    Hotfix/lvjv 72 (#72)
    * add notification by email
    
    * fix SpringAsyncConfig

--- src/main/java/com/softserve/rms/config/SpringAsyncConfig.java
+++ src/main/java/com/softserve/rms/config/SpringAsyncConfig.java
@@ -2,23 +2,16 @@ package com.softserve.rms.config;
 
 import com.softserve.rms.exceptions.handler.CustomAsyncExceptionHandler;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
 public class SpringAsyncConfig implements AsyncConfigurer {
-
-    @Bean(name = "threadPoolTaskExecutor")
-    public Executor threadPoolTaskExecutor(){
-        return new ThreadPoolTaskExecutor();
-    }
 
     @Override
     public Executor getAsyncExecutor(){
